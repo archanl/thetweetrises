90aaa8d44419f4eea1e1bb47419fda7a167645bf
Archan Luhar <archanl@gmail.com>
2014-04-13 18:06:08 +0000

    added pip requirements for tweet stream and organized setup script

--- backend/setup_backend.sh
+++ backend/setup_backend.sh
@@ -1,10 +1,10 @@
 sudo su -
 
-apt-get install redis-server
-
 apt-get install python-software-properties
 apt-add-repository ppa:chris-lea/node.js
 apt-get update
+
+apt-get install redis-server
 
 apt-get install nodejs
 
