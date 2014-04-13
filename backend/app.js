0102a0e1985e65426cd8db08b68a1239b2d3be82
Aleksander Bello <belloaleksander@gmail.com>
2014-04-13 01:55:48 -0700

    Allowing cross origin

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,12 @@ var app = require('express')()
 
 server.listen(80);
 
+app.options('/', function(req, res){ 
+    console.log("writing headers only"); 
+    res.header("Access-Control-Allow-Origin", "*"); 
+    res.end(''); 
+});
+
 app.get('/', function (req, res) {
   res.sendfile(__dirname + '/index.html');
 });
