c053b3b4a2d587dc5a70a30200cc8594abeff109
Archan Luhar <archanl@gmail.com>
2014-04-13 04:09:03 +0000

    added backend setup file for setting up new servers

---
+++ backend/setup_backend.sh
@@ -0,0 +1,13 @@
+sudo su -
+
+apt-get install redis-server
+
+apt-get install python-software-properties
+apt-add-repository ppa:chris-lea/node.js
+apt-get update
+
+apt-get install nodejs
+
+npm install socket.io express
+
+pip install redis
