788ab3b4e9c4ca0f3b83a4aebe0e41b0b954ee06
Aleksander Bello <belloaleksander@gmail.com>
2014-04-13 02:07:21 -0700

    Enable cross origin

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,22 @@ var app = require('express')()
 
 server.listen(80);
 
+var enableCORS = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+app.use(enableCORS);
+
 app.options('/', function(req, res){ 
     console.log("writing headers only"); 
     res.header("Access-Control-Allow-Origin", "*"); 
