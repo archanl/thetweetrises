1387329b6013d9bdd9c549eb2eb05d65f201db2b
Archan Luhar <archanl@gmail.com>
2014-04-18 04:42:26 +0000

    modified server start/stop/pid scripts to also run the tweet stream and categorizers

--- backend/killserver.sh
+++ backend/killserver.sh
@@ -1,2 +1,4 @@
 kill $(ps aux | grep "node app.js" | grep -v grep | awk '{print $2}')
+kill $(ps aux | grep "python tweet_stream.py" | grep -v grep | awk '{print $2}')
+kill $(ps aux | grep "python tweet_categorize.py" | grep -v grep | awk '{print $2}')
 
