513075bdf1b69bca0ef5bd13fa9688b413e6827f
Aleksander Bello <belloaleksander@gmail.com>
2014-05-02 01:09:41 -0700

    Wrong function

--- backend/app.js
+++ backend/app.js
@@ -31,19 +31,19 @@ io.sockets.on('connection', function (socket) {
   var redis_emitter = function() {
     redis_client.lrange("sentiment_stream", 0, 0, function(err, reply) {
       var point = JSON.parse(reply);
-      socket.volatile.emit('newPoint', point);
+      // Only emit if different from last message
+      if (last_emitted !== point) {
+          last_emitted = point;
+          socket.volatile.emit('newPoint', point);
+      }
     });
   };
 
   var initial_emit = function() {
     redis_client.lrange("sentiment_stream", 0, 100, function(err, reply) {
       var point = JSON.parse(reply);
+      socket.volatile.emit('preload_data', point);
       
-      // Only emit if different from last message
-      if (last_emitted !== point) {
-          last_emitted = point;
-          socket.volatile.emit('newPoint', point);
-      }
     });
   };
 
