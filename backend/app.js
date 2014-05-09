6eb163cc94f0c4581f95aefeddc4ee5b43739743
Aleksander Bello <belloaleksander@gmail.com>
2014-05-09 02:40:47 -0700

    Added function to serve last 5 trending tweets

--- backend/app.js
+++ backend/app.js
@@ -57,6 +57,20 @@ io.sockets.on('connection', function (socket) {
 
 });
 
+function getTrendingTopics() {
+    var args = ["+inf", "-inf", "WITHSCORES", "LIMIT", 0, 5];
+    var trending = 0;
+    redis_client.zrevrangebyscore(args, function(err, reply) {
+        // The trending topics will be in the even indeces of the array
+        for (i = 0; i < reply.size(); i += 2 {
+            trending += reply[i];
+        }
+    }
+    console.log(trending);
+    // TODO: serve
+
+}
+
 server.listen(8080);
 
 
