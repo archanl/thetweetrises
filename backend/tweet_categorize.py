55424477aed8fbd6e79ae9fee1d6123762a97120
Aleksander Bello <belloaleksander@gmail.com>
2014-05-15 13:11:50 -0700

    Importing time

--- backend/tweet_categorize.py
+++ backend/tweet_categorize.py
@@ -15,6 +15,7 @@ sys.path.insert(0, '../NLP/StochasticGradientDescent')
 sys.path.insert(0, '../NLP/SupportVectorMachine')
 from wrapper import classifier_wrapper, tweetclass
 from trend_utils import getTrends, classifyTrending
+import time
 
 # Log everything, and send it to stderr.
 logging.basicConfig(level=logging.DEBUG)
