ad3d6c820367c05612ab8171797a82b4453a4655
Aleksander Bello <belloaleksander@gmail.com>
2014-05-15 15:42:10 -0700

    Changed control scripts

--- backend/serverpids.sh
+++ backend/serverpids.sh
@@ -1,4 +1,5 @@
 ps aux | grep "node app.js" | grep -v grep | awk '{print $2}'
 ps aux | grep "python tweet_stream.py" | grep -v grep | awk '{print $2}'
 ps aux | grep "python tweet_categorize.py" | grep -v grep | awk '{print $2}'
+ps aux | grep "python trending_streams.py" | grep -v grep | awk '{print $2}'
 
--- backend/startserver.sh
+++ backend/startserver.sh
@@ -1,4 +1,5 @@
 node app.js > /dev/null 2>&1 &
 python tweet_stream.py > /dev/null 2>&1 &
 python tweet_categorize.py > /dev/null 2>&1 &
+python trending_streams.py > /dev/null 2>&1 &
 
