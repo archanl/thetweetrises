2b372a8a482116311674ffba316a5e14cf0a4821
Aleksander Bello <belloaleksander@gmail.com>
2014-05-16 14:11:53 -0700

    Pass instead of continue, so that we still categorize the points

--- backend/tweet_categorize.py
+++ backend/tweet_categorize.py
@@ -71,7 +71,7 @@ def main():
             tweet = json.loads(r.brpop(TRENDING_KEY)[1])
             if tweet['geo'] is None:
                 # No geo data? IGNORE!
-                continue
+                pass
 
             coordinates = tweet['geo']['coordinates']
             times = tweet['created_at']
