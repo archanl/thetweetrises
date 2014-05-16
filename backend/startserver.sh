5096aca3cf02d1362de3fff8dde75538ce223e36
Archan Luhar <archanl@gmail.com>
2014-05-15 23:17:35 -0400

    start server echo messages for info

--- backend/startserver.sh
+++ backend/startserver.sh
@@ -1,6 +1,9 @@
+echo "Killing old servers if any"
 ./killserver.sh
+echo "Starting servers..."
 node app.js > /dev/null 2>&1 &
 python tweet_stream.py > /dev/null 2>&1 &
 python tweet_categorize.py > /dev/null 2>&1 &
 python trending_streams.py > /dev/null 2>&1 &
+echo "Done starting servers..."
 
