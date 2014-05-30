a46859526ec92afd6a6de998a1705e14117e77c4
Archan Luhar <archan@caltech.edu>
2014-05-29 19:18:48 -0700

    no more trending stream

--- backend/startserver.sh
+++ backend/startserver.sh
@@ -4,6 +4,6 @@ echo "Starting servers..."
 node app.js > /dev/null 2>&1 &
 python tweet_stream.py > /dev/null 2>&1 &
 python tweet_categorize.py > /dev/null 2>&1 &
-python trending_streams.py > /dev/null 2>&1 &
+# python trending_streams.py > /dev/null 2>&1 &
 echo "Done starting servers..."
 
